f4b2da40434726bbd9366fa5aa466ef576fa00be
Striganov <vitek161147@yandex.ru>
2025-02-11 07:19:40 +0300

    добавляет реализацию

--- conv.py
+++ conv.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from pathlib import Path
+from lxml import etree
 import argparse
 import sys
 
@@ -13,11 +14,61 @@ def init() -> argparse.ArgumentParser:
     return parser
 
 
+def remove_node(parent, name):
+    """Удаляет из указанного родителя тег с переданным именем."""
+    node_for_remove = parent.find(name)
+    if node_for_remove != None:
+        parent.remove(node_for_remove)
+
+
+def save_result(dom, src_file_path: Path):
+    """Сохраняет результат обработки рядом с исходным файлом в каталоге `converted`."""
+    dst_path = Path(src_file_path.parent) / 'converted'
+    if not dst_path.exists():
+        dst_path.mkdir()
+    dst_file_path = str(dst_path / file_path.name)
+    f = open(dst_file_path, "w", encoding='cp1251', errors=None, newline='\r\n')
+    f.write(etree.tostring(dom, pretty_print=True, encoding='Windows-1251', xml_declaration=True).decode('cp1251'))
+    f.close()
+
 def prepare_prks(file_path: Path):
-    """."""
+    """Преобразует и сохраняет измененный файл prks."""
+    tree = etree.parse(str(file_path))
+    root = tree.getroot()
+
+    root.find('ZGLV').find('VERSION').text = '1.0'
+    for pers in root.findall('PERS'):
+        for node_name in ('MD_DEP_ID', 'AREA_TYPE', 'DOC_ID'):
+            remove_node(pers, node_name)
+
+        etree.SubElement(pers, "VPOLIS").text = '3'
+        etree.SubElement(pers, "SPOLIS")
+        etree.SubElement(pers, "NPOLIS").text = pers.find('ENP').text
+        remove_node(pers, 'ENP')
+
+    save_result(root, file_path)
+    print('Done.')
 
 def prepare_ozps(file_path: Path):
-    """."""
+    """Преобразует и сохраняет измененный файл ozps."""
+    tree = etree.parse(str(file_path))
+    root = tree.getroot()
+    
+    root.find('ZGLV').find('VERSION').text = '1.0'
+
+    inner_fname = root.find('ZGLV').find('FILENAME').text
+    inner_fname = inner_fname.split('.')[0] if '.xml' in inner_fname.lower() else inner_fname
+    root.find('ZGLV').find('FILENAME').text = inner_fname
+
+    for pers in root.findall('PERS'):
+        etree.SubElement(pers, "VPOLIS").text = '3'
+        etree.SubElement(pers, "SPOLIS")
+        etree.SubElement(pers, "NPOLIS").text = pers.find('ENP').text
+        remove_node(pers, 'ENP')
+
+    save_result(root, file_path)
+    print('Done.')
+
 
 if __name__ == '__main__':
     parser = init()
@@ -33,8 +84,8 @@ if __name__ == '__main__':
         print(f'handling file not found, current path "{file_path}" is directory.')
         sys.exit()
 
-    if file_path.name.lower() == 'prks':
+    if file_path.name.lower().startswith('prks'):
         prepare_prks(file_path)
 
-    elif file_path.name.lower() == 'ozps':
+    elif file_path.name.lower().startswith('ozps'):
         prepare_ozps(file_path)
